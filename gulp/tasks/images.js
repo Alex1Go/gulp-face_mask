bfc59fc6c823ca5c11470feb23dcd1f195c88032
Oleksii Hostryk <125027886+Alex1Go@users.noreply.github.com>
2025-08-30 15:33:31 +0300

    gulp change

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,7 +1,30 @@
+// import gulpif from "gulp-if";
+// import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
+// import newer from "gulp-newer";
+
+// export const images = () => {
+//   return app.gulp
+//     .src([`${app.paths.srcImgFolder}/**/*.{jpg,jpeg,png,svg}`], {
+//       encoding: false,
+//     })
+//     .pipe(newer(app.paths.buildImgFolder))
+//     .pipe(
+//       gulpif(
+//         app.isProd,
+//         imagemin([
+//           gifsicle({ interlaced: true }),
+//           mozjpeg({ quality: 90, progressive: true }),
+//           optipng({ optimizationLevel: 1 }),
+//         ])
+//       )
+//     )
+//     .pipe(app.gulp.dest(app.paths.buildImgFolder));
+// };
 import gulpif from "gulp-if";
 import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
 import newer from "gulp-newer";
 
+// Копирование и оптимизация изображений (jpg, jpeg, png, svg)
 export const images = () => {
   return app.gulp
     .src([`${app.paths.srcImgFolder}/**/*.{jpg,jpeg,png,svg}`], {
@@ -15,6 +38,7 @@ export const images = () => {
           gifsicle({ interlaced: true }),
           mozjpeg({ quality: 90, progressive: true }),
           optipng({ optimizationLevel: 1 }),
+          svgo(),
         ])
       )
     )
--- gulp/tasks/webp.js
+++ gulp/tasks/webp.js
@@ -1,5 +1,13 @@
 import webp from "gulp-webp";
 
+// export const webpImages = () => {
+//   return app.gulp
+//     .src([`${app.paths.srcImgFolder}/**/*.{jpg,jpeg,png}`], { encoding: false })
+//     .pipe(webp())
+//     .pipe(app.gulp.dest(app.paths.buildImgFolder));
+// };
+
+// Генерация webp-изображений
 export const webpImages = () => {
   return app.gulp
     .src([`${app.paths.srcImgFolder}/**/*.{jpg,jpeg,png}`], { encoding: false })
